dc4c07c071e8090164201c7b239412b7de3ff77a
tikisi <faohfh@gmail.com>
2020-03-24 02:40:51 +0900

    動きを滑らかに

--- Player.cpp
+++ Player.cpp
@@ -1,30 +1,39 @@
 #include "Player.h"
 
-#include "MapManager.h"
 #include "Attack.h"
+#include "MapManager.h"
 
 void Player::update() {
   // 移動処理
-  Point nextPos = pos;
-  if (KeyUp.down()) {
-    nextPos += Point::Up(1);
-    this->dir = Direction::Up;
+  if (moveCounter == 0) {
+    this->nextPos = pos;
+    if (KeyUp.down()) {
+      nextPos += Point::Up(1);
+      this->dir = Direction::Up;
+    }
+    if (KeyDown.down()) {
+      nextPos += Point::Down(1);
+      this->dir = Direction::Down;
+    }
+    if (KeyRight.down()) {
+      nextPos += Point::Right(1);
+      this->dir = Direction::Right;
+    }
+    if (KeyLeft.down()) {
+      nextPos += Point::Left(1);
+      this->dir = Direction::Left;
+    }
+    if (mapMediator->isEmpty(nextPos) && pos != nextPos) {
+      // this->pos = nextPos;
+      this->moveCounter = 1;
+    }
   }
-  if (KeyDown.down()) {
-    nextPos += Point::Down(1);
-    this->dir = Direction::Down;
-  }
-  if (KeyRight.down()) {
-    nextPos += Point::Right(1);
-    this->dir = Direction::Right;
-  }
-  if (KeyLeft.down()) {
-    nextPos += Point::Left(1);
-    this->dir = Direction::Left;
-  }
-
-  if (mapMediator ->isEmpty(nextPos)) {
-    this->pos = nextPos;
+  if (moveCounter != 0) {
+    this->drawPos += 2 * getVecFromDir(this->dir);
+    if (moveCounter++ == 16) {
+      this->moveCounter = 0;
+      this->pos = this->nextPos;
+    }
   }
 
   // ノックバック
@@ -35,18 +44,17 @@ void Player::update() {
   }
 
   // 攻撃
-  if(KeyZ.down()) {
-    mapMediator->addAttack(new StraightAttack(this->pos, this->dir)); 
+  if (KeyZ.down()) {
+    mapMediator->addAttack(new StraightAttack(this->pos, this->dir));
   }
 }
 
-void Player::laterUpdate() {
-  this->mapMediator->setDirt(this->pos);
-}
+void Player::laterUpdate() { this->mapMediator->setDirt(this->pos); }
 
 void Player::draw() const {
   if (knockBackCounter == 0 || !(knockBackCounter % 10 == 0)) {
-    Rect(pos * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE).draw(Palette::Gold);
+    // Rect(pos * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE).draw(Palette::Gold);
+    Rect(drawPos, BLOCK_SIZE).draw(Palette::Red);
   }
 }
 
--- Player.h
+++ Player.h
@@ -10,16 +10,26 @@ class Player {
  private:
   MapMediator *mapMediator;
   Point pos;
+  Point drawPos;
+  Point nextPos;
   Direction dir;
   int knockBackCounter;
+  int moveCounter;  // 0: 移動していない 0以外: 移動中
 
  public:
   Player(Point pos, Direction dir, MapMediator *mapMediator)
-      : pos(pos), dir(dir), mapMediator(mapMediator){
+      : pos(pos), dir(dir), mapMediator(mapMediator) {
+    this->moveCounter = 0;
+    this->drawPos = pos * BLOCK_SIZE;
   };
+
   void update();
-  void laterUpdate(); // 当たり判定後の更新
+
+  void laterUpdate();  // 当たり判定後の更新
+
   void draw() const;
+
   Point getPos() const { return this->pos; }
+
   void setKnockBack();
 };
